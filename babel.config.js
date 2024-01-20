581de7fd9d3ec2591db4cab4eba02538cb1c9a1d
Isakiel Souza <isakielsouza@outlook.com.br>
2024-01-20 16:23:14 -0300

    feat: Configurado package e paths

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,20 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@Assets': './src/Assets',
+            '@Components': './src/Components',
+            '@Routes': './src/Routes',
+            '@Modules': './src/Modules',
+            '@Storage': './src/Storage',
+          }
+        }
+      ]
+    ]
   };
 };
