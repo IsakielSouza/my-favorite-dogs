12a4d000f7b145337d01a309b18804be4915a18e
Isakiel Souza <isakielsouza@outlook.com.br>
2024-01-20 16:40:46 -0300

    feat: Criado pages SignIn, SignUp,StatusBar Loadin

--- App.tsx
+++ App.tsx
@@ -1,16 +1,23 @@
-import { ActivityIndicator } from 'react-native';
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { Loading } from '@Components/Loading';
 
 import theme from './src/theme'
-import { SignUp } from '@Modules/Authentication/Presentation/Pages/SignUp/SignUp';
+
+import { SignIn } from '@Modules/Authentication/Presentation/Pages/SignIn/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ? <SignUp/> : <ActivityIndicator /> }
+      <StatusBar 
+          barStyle="dark-content"
+          backgroundColor="transparent"
+          translucent
+        />
+      { fontsLoaded ? <SignIn/> : <Loading /> }
     </ThemeProvider>
     
   );
