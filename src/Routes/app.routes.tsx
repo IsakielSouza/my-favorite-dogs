97fa11fcb79b2119d753d866fad250aeb34e845e
Isakiel Souza <isakielsouza@outlook.com.br>
2024-01-22 12:58:37 -0300

    feat: Finalizado icons e navegacao tabRoutes

--- src/Routes/app.routes.tsx
+++ src/Routes/app.routes.tsx
@@ -1,4 +1,7 @@
+import { Platform } from 'react-native'
 import { createBottomTabNavigator, BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import { useTheme } from 'styled-components/native';
+import Icon from '@expo/vector-icons/Feather'
 
 import Home from '@Modules/Dog/Pages/Home';
 import Favorite from '@Modules/Dog/Pages/Favorite';
@@ -17,23 +20,56 @@ export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes(){
+
+  const { FONT_SIZE, COLORS } = useTheme();
+
+  const iconSize = FONT_SIZE['MD'];
+
   return(
-    <Navigator screenOptions={{headerShown: false}}>
+    <Navigator screenOptions={{
+      headerShown: false,
+      tabBarShowLabel: false,
+      tabBarActiveTintColor: COLORS.WHITE,
+      tabBarInactiveTintColor: COLORS.PURPLE_DARK,
+      tabBarStyle: {
+        backgroundColor: COLORS.VIOLET_700,
+        borderTopWidth: 0,
+        height: Platform.OS === "android" ? 70 : 96,
+        paddingBottom: 10, 
+        paddingTop: 6 
+      }
+    }}>
       <Screen 
         name="home"
         component={Home}
+        options={{
+          tabBarIcon: ({ color  }) => (
+            <Icon name='home' color={color} size={iconSize} />
+          )
+        }}
       />
       <Screen 
         name="favorite"
         component={Favorite}
+        options={{
+          tabBarIcon: ({ color  }) => (
+            <Icon name={'heart'} color={color} size={iconSize} />
+          )
+        }}
       />
       <Screen 
         name="profile"
         component={Profile}
+        options={{
+          tabBarIcon: ({ color  }) => (
+            <Icon name={'user'} color={color} size={iconSize} />
+          )
+        }}
       />
       <Screen 
         name="details"
         component={Details}
+        options={{ tabBarButton: () => null }}
       />
     </Navigator>
   );
